105a0538b5139936717f077a18723695fbed87e3
ibrahim AKIN <ibrahim.akin@ithero.com.tr>
2021-04-28 10:59:00 +0300

    Update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const lang = {
     logi: /(%=|%|\-|\+|\*|&amp;{1,2}|\|{1,2}|&lt;=|&gt;=|&lt;|&gt;|!={1,2}|={2,3})/g,
     numb: /(\d+(\.\d+)?(e\d+)?)/g,
     func: /(?<=^|\s*)(async|console|alert|Math|Object|Array|String|class(?!\s*\=)|function|(?<=\.)\D\w*)(?=\b)/g,
-    decl: /(?<=^|\s*)(var|let|const)/g, // Declarations
+    decl: /(?<=^|\s*)(var|let|const)/g,                                           // Declarations
     pare: /(\(|\))/g,
     squa: /(\[|\])/g,
     curl: /(\{|\})/g
@@ -20,7 +20,7 @@ const lang = {
   }
 };
 
-let iframeOMid= 'OM300x250'+Math.round(Math.random()*(10000-1))+1; // create multi unique iframe
+let iframeOMid= 'OM300x250'+Math.round(Math.random()*(10000-1))+1;                // create multi unique iframe
 let iframeOM = document.createElement('IFRAME');
 iframeOM.id = iframeOMid;
 iframeOM.src = 'about:blank';
@@ -31,31 +31,51 @@ iframeOM.scrolling='no';
 iframeOM.margin='0';
 iframeOM.frameborder='0';
 
-const resultFrame = document.getElementById('code-result');
-resultFrame.appendChild(iframeOM);                          // this can not apply to curent div
-const codeResult = resultFrame.children[0];
-
-const replaceChar = (innerHtml) => { return innerHtml.replace(/<div>/g, '').replace(/<\/div>/g, '').replace(/&lt;/g, '<').replace(/&gt;/g, '>'); }
+const codeResult =  document.getElementById('code-result').appendChild(iframeOM); // this can not apply to curent div
 const editors = document.querySelectorAll('.highLite_editable');
+const runButton = document.getElementById('run-button');
+const instantRun = document.getElementById('instant-run');
+
+const replaceChar = (innerHtml) => {
+  return innerHtml.replace(/<div>/g, '').replace(/<\/div>/g, '').replace(/<br>/g, '').replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+};
+
+let insantCompile = false;
 let htmlResult = replaceChar(editors[0].innerHTML);
 let scriptResult = replaceChar(editors[1].innerHTML);
 
+const insertResults = () => {
+  let contentDOM = '<html><body>' + htmlResult + '<script type="text/javascript">' + scriptResult + '</script></body></html>';
+  codeResult.src = 'data:text/html;charset=utf-8,' + escape(contentDOM);
+  // Another way to write iFrame
+  // let docu = iframeOM.contentWindow.document;
+  // docu.open(); docu.writeln(contentDOM); docu.close();
+}
 
-const highLite = el => {
-  const dataLang = el.dataset.lang; // Detect "js", "html", "py", "bash", ...
-  const langObj = lang[dataLang]; // Extract object from lang regexes dictionary
+const handleRunClick = (event) => {
+  htmlResult = replaceChar(editors[0].innerHTML);
+  scriptResult = replaceChar(editors[1].innerHTML);
+  insertResults();
+};
+
+const handleCheckbox = (event) => {
+  insantCompile = event.target.checked;
+}
+
+const highLite = (el) => {
+  const dataLang = el.dataset.lang;                                               // Detect "js", "html", "py", "bash", ...
+  const langObj = lang[dataLang];                                                 // Extract object from lang regexes dictionary
   let html = el.innerHTML;
   Object.keys(langObj).forEach(function(key) {
     html = html.replace(langObj[key], `<i class=${dataLang}_${key}>$1</i>`);
   });
-  el.previousElementSibling.innerHTML = html; // Finally, show highlights!
+  el.previousElementSibling.innerHTML = html;                                     // Finally, show highlights!
 
-  if(dataLang === 'js'){ scriptResult = replaceChar(el.innerHTML); }
-  else if(dataLang === 'html'){ htmlResult = replaceChar(el.innerHTML); }
-
-  let contentDOM = '<html><body>' + htmlResult + '<script type="text/javascript">' + scriptResult + '</script></body></html>';
-  codeResult.src = 'data:text/html;charset=utf-8,' + escape(contentDOM);
-  // let docu = iframeOM.contentWindow.document; docu.open(); docu.writeln(contentDOM); docu.close();
+  if (insantCompile) {  
+    if(dataLang === 'js'){ scriptResult = replaceChar(el.innerHTML); }
+    else if(dataLang === 'html'){ htmlResult = replaceChar(el.innerHTML); }
+    insertResults();
+  }
 };
 
 editors.forEach(el => {
@@ -64,7 +84,9 @@ editors.forEach(el => {
   el.autocorrect = 'off';
   el.autocapitalize = 'off';
   el.addEventListener('input', highLite.bind(null, el));
-  highLite(el); // Init!
+  highLite(el);                                                                   // Init!
 });
 
+runButton.addEventListener('click', handleRunClick);
+instantRun.addEventListener('change', handleCheckbox);
 // https://stackoverflow.com/questions/41884969/highlight-syntax-in-contenteditable
